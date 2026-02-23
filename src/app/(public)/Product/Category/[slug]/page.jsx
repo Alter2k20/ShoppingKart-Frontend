71af187ec37877cd1450e2863986a44f29b3ef8e
Alter2k20 <mishraashutosh98611@gmail.com>
2026-02-23 10:18:06 +0530

    added dynamic rout get data by product id

---
+++ src/app/(public)/Product/Category/[slug]/page.jsx
@@ -0,0 +1,11 @@
+
+import { getProductsByCategory } from "@/lib/api/product";
+
+
+export default function Category(){
+    // const data = getProductsByCategory(URL.toString().split("/")[3])
+    return (
+        <div className="text-black">
+        </div>
+    )
+}
--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,8 +1,13 @@
 "use client"
-import { useEffect, useState } from "react"; 
+import { useEffect, useState } from "react";
 import { getProducts } from "@/lib/api/product";
 
+import Link from "next/link";
+
 import Card from "@/components/general/Card";
+
+import category from "@/lib/dummy/static.json";
+
 
 export default function Home() {
   const [data, setData] = useState(null);
@@ -22,16 +27,27 @@ export default function Home() {
   }, []);
 
   return (
-    <div className=" grid grid-cols-4 place-items-center p-4 font-sans overflow-y-scroll gap-2">
-      {/* <main> */}
-            {data ? (
-      data.map((product, index) => (
-        <Card key={index} product={product} />
-      ))
-    ) : (
-      <p>Loading...</p>
-    )}
-      {/* </main> */}
+    <div>
+      <div className="flex w-screen p-4 gap-2 ">
+        {
+          category.map((type, index) => (
+            <Link key={index} href={`/Product/Category/${type.id}`} >
+            <div className=" button duration-500 text-center p-4 border rounded h-fit w-fit"> {type.name} </div>
+            </Link>
+          ))
+        }
+      </div>
+      <div className=" grid grid-cols-4 place-items-center p-4 font-sans overflow-y-scroll gap-2">
+        {/* <main> */}
+        {data ? (
+          data.map((product, index) => (
+            <Card key={index} product={product} />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+        {/* </main> */}
+      </div>
     </div>
   );
 }
